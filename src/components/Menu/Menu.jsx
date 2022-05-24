68d54c886ea344494f932854ea720547031d36bf
João Cruz <joao.cruz@redspark.io>
2022-05-24 14:21:34 -0300

    Adição do modal de produtos

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,62 +1,150 @@
-import { Avatar, Box, Container, Grid, Paper, Stack, Typography } from '@mui/material'
-import React from 'react'
+import { Container, Dialog, Grid, Paper, Typography } from '@mui/material'
+import React, { useState } from 'react'
 import { connect } from 'react-redux';
 import { styled } from '@mui/material/styles';
-import { CategorieName, Product, ProductName } from './styles';
+import { CategorieName, Product, ProductName, WrapperModal, Img, H1, Description, WrapperImg, Header, Body, CustomButton, SessionTitle } from './styles';
+import MuiAccordion from '@mui/material/Accordion';
+import MuiAccordionSummary from '@mui/material/AccordionSummary';
+import MuiAccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    minHeight: "10.375rem",
-    maxHeight: "10.375rem",
-    color: theme.palette.text.secondary,
-    'img': {
-        marginTop: "-30px",
-        width: "100%"
-    }
+	backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+	...theme.typography.body2,
+	display: 'flex',
+	flexDirection: 'column',
+	justifyContent: 'space-between',
+	padding: theme.spacing(1),
+	textAlign: 'center',
+	minHeight: "11.375rem",
+	maxHeight: "11.375rem",
+	color: theme.palette.text.secondary,
+	cursor: "pointer"
+}));
+
+const Accordion = styled((props) => (
+	<MuiAccordion disableGutters elevation={0} square {...props} />
+))(({ theme }) => ({
+	border: `2px solid ${theme.palette.divider}`,
+	borderRadius: '8px',
+	'&:not(:last-child)': {
+		borderBottom: '',
+	},
+	'&:before': {
+		display: 'none',
+	},
+	'&.selected': {
+		border: `1px solid #ED8B26`,
+	}
+}));
+
+const AccordionSummary = styled((props) => (
+	<MuiAccordionSummary {...props} />
+))(({ theme }) => ({
+	padding: 0,
+	margin: 0,
+	backgroundColor: '#efefef'
+}));
+
+const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+	padding: theme.spacing(2),
+	borderTop: '1px solid rgba(0, 0, 0, .125)',
+	backgroundColor: '#efefef'
 }));
 
 function Feed(props) {
-    const { categories } = props;
-    return (
-        <Container disableGutters component="main" sx={{ pt: 4, pb: 2, pl: 2, pr: 2 }}>
-            {categories && categories.map((categorie) => {
-                return (
-                    <>
-                        <CategorieName>
-                            {categorie.name}
-                        </CategorieName>
-                        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                            {categorie && categorie?.products.map((product) => {
-                                return (
-                                    <>
-                                        <Grid item xs={6} md={2}>
-                                            <Item>
-                                                <img src={product.image} />
-                                                <Product>
-                                                    <ProductName>{product.name}</ProductName>
-                                                </Product>
-                                            </Item>
-                                        </Grid>
-                                    </>
-                                )
-                            })}
-                        </Grid>
-                    </>
-                )
-            })}
-        </Container>
-    )
+	const { categories } = props;
+	const [open, setOpen] = useState(false);
+	const [selectedProduct, setSelectedProduct] = useState(null);
+
+	const handleClose = () => {
+		setSelectedProduct(null)
+		setOpen(false)
+	}
+
+	const handleDialogSelectProduct = (product) => {
+		setSelectedProduct(product)
+		setOpen(true)
+	}
+
+	return (
+		<Container disableGutters component="main" sx={{ pt: 4, pb: 2, pl: 2, pr: 2 }}>
+			<SessionTitle>Nosso Cardápio</SessionTitle>
+			{categories && categories.map((categorie) => {
+				return (
+					<>
+						<CategorieName>
+							{categorie.name}
+						</CategorieName>
+						<Grid container rowSpacing={1} sx={{ pb: 4 }} columnSpacing={{ xs: 2, sm: 2, md: 4 }}>
+							{categorie && categorie?.products.map((product) => {
+								return (
+									<>
+										<Grid item xs={6} md={2}>
+											<Item onClick={() => handleDialogSelectProduct(product)}>
+												<Product>
+													<img src={product.image} alt="Imagem produto" />
+													<ProductName>{product.name}</ProductName>
+												</Product>
+											</Item>
+										</Grid>
+									</>
+								)
+							})}
+						</Grid>
+					</>
+				)
+			})}
+
+			{selectedProduct &&
+				<Dialog
+					onClose={handleClose}
+					open={open}
+					PaperProps={{
+						elevation: 0,
+						style: {
+							overflowY: 'unset',
+							maxWidth: '20rem',
+							minWidth: '20rem'
+						}
+					}}
+				>
+					<WrapperModal>
+						<WrapperImg>
+							<Img src={selectedProduct.image} alt="Imagem produto" />
+						</WrapperImg>
+						<H1>{selectedProduct.name}</H1>
+						<Description>{selectedProduct.description}</Description>
+						<Accordion>
+							<AccordionSummary
+								expandIcon={<ExpandMoreIcon sx={{ color: '#949494' }} />}
+								aria-controls={`panel1a-content-${selectedProduct.id}`}
+								id={`panel1a-header-${selectedProduct.id}`}
+							>
+								<Header>Informações Nutricionais</Header>
+							</AccordionSummary>
+							<AccordionDetails>
+								<Body
+								>
+									{selectedProduct.nutritionalTable}
+								</Body>
+							</AccordionDetails>
+						</Accordion>
+						<CustomButton>
+							Quero Agora!
+						</CustomButton>
+					</WrapperModal>
+				</Dialog>}
+		</Container>
+	)
 }
 
 const mapStateToProps = (state) => {
-    return {
-        categories: state.menu.categories || []
-    };
+	return {
+		categories: state.menu.categories || []
+	};
 }
 
 export default connect(
-    mapStateToProps, {
+	mapStateToProps, {
 })(Feed);
--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -1,8 +1,16 @@
 import styled from 'styled-components'
 
+export const SessionTitle = styled.h1`
+	font-size: 3rem;
+	width: 100%;
+	text-align: center;
+	color: ${props => props.theme.colors.primary};
+`
+
 export const CategorieName = styled.div`
-    font-size: 2.6rem;    
+    font-size: 2rem;    
     color: ${props => props.theme.colors.primary};
+    margin-top: 1.5rem;
     margin-bottom: 1.5rem;
 `
 export const Product = styled.div`
@@ -10,12 +18,85 @@ export const Product = styled.div`
     flex-direction: column;
     justify-content: end;
     height: 100%;
-    padding: 0 0 2rem 0;
+
+    > img {
+        transform: scale(1.1);
+        margin: .6rem 0; 
+        transition: transform .2s;
+
+        :hover {
+            transform: scale(1.2); 
+        }
+    }
 `
-export const ProductName = styled.div`
-    font-size: 18px;    
+export const ProductName = styled.p`
+    font-size: 17px;    
     color: ${props => props.theme.colors.primary};
     text-align: center;
-    font-weight: 400;
-    bottom: 0;
+    font-weight: 500;
+    padding: 0;
+    margin: 0;
+`
+export const WrapperModal = styled.div`
+    padding: 1rem 2rem;
+
+    >h1 {
+        color: ${props => props.theme.colors.primary};
+        font-size: 18px;
+        font-weight: 400;
+        text-align: center;
+    }
+`
+export const WrapperImg = styled.div`
+		min-height: 11rem;
+		max-height: 11rem;
+
+		> img {
+			width: 100%;
+			transform: scale(1.4);
+			margin-top: -40px;
+		}
+`
+
+export const Img = styled.img``
+
+export const H1 = styled.h1``
+
+export const Description = styled.p`
+    padding: 0;
+    margin: 0;
+    color: ${props => props.theme.colors.textPrimary};
+    text-align: center;
+    font-size: 14px;
+		margin-bottom: 1rem;
+`
+export const Header = styled.div`
+		font-size: 14px;
+		font-weight: bold;
+		text-align: center;
+		color: ${props => props.theme.colors.textPrimary};
+		width: 100%;
+`
+export const Body = styled.div`
+		font-size: 14px;
+		text-align: center;
+		color: ${props => props.theme.colors.textPrimary};
+		width: 100%;
+`
+export const CustomButton = styled.button`
+    color: #fff;
+    padding: 10px 16px;
+    border-radius: 4px;
+    font-weight: 100;
+    outline: 0;
+    border: 0;
+    margin: 1rem 0 0 0;
+		text-align: center;
+    font-size: 0.875rem;
+    line-height: 2;
+    letter-spacing: 0.02857em;
+    text-transform: uppercase;
+    min-width: 64px;
+		width: 100%;
+    background-color: ${props => props.theme.colors.primary};
 `
