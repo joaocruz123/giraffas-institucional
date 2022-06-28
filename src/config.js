9f4ee63ca5c52a1f77b3bebf629c8cf3c6889664
João Paulo Franco da Cruz <joaofranco.cruz@gmail.com>
2022-06-28 13:50:33 -0300

    Correção do app id

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ export const TITLE= process.env.REACT_APP_TITLE || 'Giraffas'
 export const API_URL = process.env.REACT_APP_API_URL || 
 	//'https://app-voceqpad-stage.azurewebsites.net/api/api/'
 	'https://www.voceqpad.com.br/api/api'
-export const APP_ID = process.env.REACT_APP_ID || 20
+export const APP_ID = process.env.REACT_APP_ID || 5
 export const STORE_ID = process.env.REACT_APP_STORE_ID || 1612
 export const STORES_LIMIT = process.env.REACT_APP_STORES_LIMIT || 10
 export const PLATFORM = process.env.REACT_APP_PLATFORM || 2
