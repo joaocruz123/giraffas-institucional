e2c7bdb9f3a3a53e6ec57ce472411e6ac9f79c1e
João Cruz <joao.cruz@redspark.io>
2022-06-29 02:16:13 -0300

    Correções das configs

--- src/config.js
+++ src/config.js
@@ -3,11 +3,11 @@ import Theme from './theme.json'
 export const THEME = Theme
 export const TITLE= process.env.REACT_APP_TITLE || 'Giraffas'
 export const API_URL = process.env.REACT_APP_API_URL || 
-	'https://app-voceqpad-stage.azurewebsites.net/api/api/'
-	//'https://www.voceqpad.com.br/api/api'
-export const APP_ID = process.env.REACT_APP_ID || 20
-export const STORE_ID = process.env.REACT_APP_STORE_ID || 10
+	//'https://app-voceqpad-stage.azurewebsites.net/api/api/'
+	'https://www.voceqpad.com.br/api/api'
+export const APP_ID = process.env.REACT_APP_ID || 5
+export const STORE_ID = process.env.REACT_APP_STORE_ID || 1612
 export const STORES_LIMIT = process.env.REACT_APP_STORES_LIMIT || 10
-export const PLATFORM = process.env.REACT_APP_PLATFORM || 3
+export const PLATFORM = process.env.REACT_APP_PLATFORM || 2
 export const PLATFORM_ID = process.env.REACT_APP_PLATFORM_ID || 5
 export const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY || "AIzaSyBUKUg5XlcJFnqSQ1R8phCnbRe7owwMVYI"
